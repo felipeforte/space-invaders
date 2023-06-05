64465ab2f06301f9200f4ec6f138b4d137d09071
Felipe Forte <felipeforte@protonmail.com>
2023-06-05 17:34:14 -0300

    Diretório mais simples

---
+++ src/Tiro.java
@@ -0,0 +1,34 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import pkg.Elemento;
+
+public class Tiro extends Elemento {
+
+	private boolean inimigo;
+
+	public Tiro() {
+		setLargura(5);
+		setAltura(5);
+	}
+
+	public Tiro(boolean inimigo) {
+		this();
+		this.inimigo = inimigo;
+	}
+
+	@Override
+	public void atualiza() {
+	}
+
+	@Override
+	public void desenha(Graphics2D g) {
+		if (!isAtivo())
+			return;
+
+		g.setColor(inimigo ? Color.RED : Color.WHITE);
+
+		g.fillRect(getPx(), getPy(), getLargura(), getAltura());
+	}
+
+}
